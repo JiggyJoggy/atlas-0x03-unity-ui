c5c6bc729fadbf4b941835399bf8d86a7dbe661f
JiggyJoggy <JaggerVanW@gmail.com>
2024-02-10 19:07:47 -0600

    task 0; setting up UI

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody rigid;
+    public int health;
+
+    private int score = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            rigid.AddForce(Vector3.right * speed);
+        }
+        else if (Input.GetAxis("Horizontal") < 0)
+        {
+            rigid.AddForce(-Vector3.right * speed);
+        }
+
+        if (Input.GetAxis("Vertical") > 0)
+        {
+            rigid.AddForce(Vector3.forward * speed);
+        }
+        else if (Input.GetAxis("Vertical") < 0)
+        {
+            rigid.AddForce(-Vector3.forward * speed);
+        }
+
+        if (health == 0)
+        {
+            Debug.Log("Game Over!");
+            health = 5;
+            score = 0;
+
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Pickup"))
+        {
+            score++;
+            Debug.Log($"Score: {score}");
+            Destroy(other.gameObject);
+        }
+
+        if (other.CompareTag("Trap"))
+        {
+            health--;
+            Debug.Log($"Health: {health}");
+        }
+
+        if (other.CompareTag("Goal"))
+        {
+            Debug.Log("You win!");
+        }
+    }
+}
